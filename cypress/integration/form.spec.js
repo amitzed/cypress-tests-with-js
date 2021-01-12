90beff102b21f0f75e0309b0d0f4abb9d3e294a4
amitzed <amitzmn@gmail.com>
2021-01-12 16:21:03 -0500

    used get type and should commands to test inputs

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -5,6 +5,12 @@ describe('Form Testing Suite', () => {
   it('Form can be filled', () => {
     cy.visit('/');
     cy.get('form');
+
+    cy.get('#name').type('Amit');
+    cy.get('#email').type('amit@gmail.com');
+    cy.get('#message').type('Testing this using Cypress');
+
+    cy.get('button').should('contain', 'SEND');
   });
 
 });
