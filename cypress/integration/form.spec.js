f50d3cd32d90a1c117ac8219df56044eef87db0d
amitzed <amitzmn@gmail.com>
2021-01-12 16:56:48 -0500

    Added second page to test if link works

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -6,11 +6,17 @@ describe('Form Testing Suite', () => {
     cy.visit('/');
     cy.get('form');
 
+    cy.get(':nth-child(1) > label').should('contain', 'Name');
+
     cy.get('#name').type('Amit');
     cy.get('#email').type('amit@gmail.com');
     cy.get('#message').type('Testing this using Cypress');
 
     cy.get('button').should('contain', 'SEND');
+
+    cy.get('form').submit();
+    
+    cy.get('#second-page-link').click().url().should('contain', '/secondPage');
   });
 
 });
